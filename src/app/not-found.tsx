c080e6344a4393ccdbeda77b151ed83424bb5b08
valentine231 <ugwuvalentine519@gmail.com>
2026-01-25 15:12:55 +0100

    added-not-found.tsx-file

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+
+export const dynamic = "force-dynamic";
+
+export default function NotFound() {
+    return (
+        <div className="flex flex-col justify-center items-center h-screen">
+
+                <h1 className="text-red-2xl font-bold">404</h1>
+                <p className="text-2xl text-red">page not found</p>
+            
+        </div>
+    )
+
+}
